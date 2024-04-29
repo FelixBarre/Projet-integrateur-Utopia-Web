57387017174495bdc90f25dfdec89c511846239d
jaypomaloney <mathisl525@gmail.com>
2024-04-29 15:33:16 -0400

    Seeders
    Compte bancaire, Type Demande,  Etat Demande

---
+++ database/seeders/EtatDemandeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EtatDemandeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('etat_demandes')->insert([
+            ['label' => 'Approuvée'],
+            ['label' => 'Refusée'],
+            ['label' => 'En attente'],
+            ['label' => 'Annulée']
+        ]);
+    }
+}
