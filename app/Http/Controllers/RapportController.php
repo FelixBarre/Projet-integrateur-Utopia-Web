c3c7f06ac335e819940c2fb631730858b3844edc
Felix Barre <202263237@cegepsherbrooke.qc.ca>
2024-04-30 08:59:51 -0400

    Ajout middleware EnsureUserIsAdmin

--- app/Http/Controllers/RapportController.php
+++ app/Http/Controllers/RapportController.php
@@ -12,7 +12,9 @@ class RapportController extends Controller
      */
     public function index()
     {
-        //
+        return view('rapport.rapports', [
+            'rapports' => Rapport::All()
+        ]);
     }
 
     /**
