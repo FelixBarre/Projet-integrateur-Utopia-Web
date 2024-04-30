10000d4c491cb793697797d93f97b64ee96a1735
Joel Tidjane <tidjanejoel@gmail.com>
2024-04-30 00:21:36 -0400

    Ajout des differents boutons, form, filtre et donnees de transactions à la page d'accueil

--- app/Models/CompteBancaire.php
+++ app/Models/CompteBancaire.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CompteBancaire extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    public function transaction(): HasMany
+    {
+        return $this->HasMany(Transaction::class, 'id_user');
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function comptes() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_compte_envoyeur');
+    }
+
+    public function type_transactions() : BelongsTo
+    {
+        return $this->belongsTo(TypeTransaction::class, 'id_type_transaction');
+    }
+
+    public function etat_transactions() : BelongsTo
+    {
+        return $this->belongsTo(EtatTransaction::class, 'id_etat_transaction');
+    }
+
+
+
 }
