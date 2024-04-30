7b17207ef2fd7ca3053082f2c090345d52303b1c
jaypomaloney <mathisl525@gmail.com>
2024-04-30 11:04:41 -0400

    CRUD compte bancaire
    Création des routes dans l'API ainsi que les méthodes liées aux CRUD dans le controller. Création du CompteBancaireRessource.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompteBancaireController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(CompteBancaireController::class)->group(function() {
+    Route::get('/comptesBancaires', 'index')->name('comptesBancairesApi');
+    Route::get('/compteBancaire', 'show')->name('compteBancaireApi');
+    Route::post('/creation/compteBancaire', 'store')->name('creationCompteBancaireApi');
+    Route::put('/modification/compteBancaire', 'update')->name('modificationCompteBancaireApi');
+    Route::delete('/desactivation/compteBancaire', 'destroy')->name('desactivationCompteBancaireApi');
+});
