f97147921d1e38e6529e842f7d290792d017d37d
Joel Tidjane <tidjanejoel@gmail.com>
2024-05-03 14:34:51 -0400

    Ajout de js pour les filtres et modification de l'API pour les transactions

--- resources/views/accueil/accueil.blade.php
+++ resources/views/accueil/accueil.blade.php
@@ -3,7 +3,7 @@
         <div class="flex flex-row"><!-- Premier bloc contenant message, infos et datetime -->
 
                 <div class="w-2/4 text-left">
-                    <h2 class="text-6xl font-bold text-white mx-9">Bonjour !</h2>
+                    <h2 class="text-6xl font-bold text-white mx-9" id="p">Bonjour !</h2>
                 </div>
 
                 <div class="w-2/4 text-right text-white">
@@ -27,7 +27,9 @@
                 @csrf
                 <p class="my-5 mx-9">
                     <label for="filter" class="text-lg text-white">Filtrer par </label>
+
                     <select id="selectValue" name="id_type_Transaction" class="w-64 p-3">
+                        <option value="none" id="optionValue">Sélectionner</option>
                         @foreach ($type_transactions as $type_transaction)
                         <option value="{{$type_transaction->id}}" id="optionValue">{{$type_transaction->label}}</option>
                         @endforeach
@@ -49,7 +51,7 @@
                     <th class="w-1/6 p-4 m-auto border-2 border-solid">IdDemande</th>
                     <th class="w-1/6 m-auto border-2 border-solid">Tâche</th>
                     <th class="w-1/6 m-auto border-2 border-solid">Nom du client</th>
-                    <th class="w-1/6 m-auto border-2 border-solid">Téléphone</th>
+                    <th class="w-1/6 m-auto border-2 border-solid">E-mail</th>
                     <th class="w-1/6 m-auto border-2 border-solid">Date</th>
                     <th class="w-1/6 m-auto border-2 border-solid">Status</th>
                     </tr>
@@ -59,9 +61,13 @@
 
                     @endphp
                     @foreach ($transactions as $transaction)
-                        @if ($transaction->etat_transactions->label != "En cours")
+                        @if ($transaction->etat_transactions->label == "Terminé")
                             @php
                                 $class_value = " bg-green-500 ";
+                            @endphp
+                        @elseif ($transaction->etat_transactions->label == "Annulé")
+                            @php
+                                $class_value = " bg-red-500 ";
                             @endphp
                         @else
                             @php
@@ -73,16 +79,12 @@
                     <td class="m-auto text-center bg-white border-2 border-solid">{{$transaction->type_transactions->label}}</td>
                     <td class="m-auto text-center bg-white border-2 border-solid">{{$transaction->comptes_bancaire->comptes->nom}}</td>
                     <td class="m-auto text-center bg-white border-2 border-solid">{{$transaction->comptes_bancaire->comptes->email}}</td>
-                    <td class="m-auto text-center bg-white border-2 border-solid">{{$transaction->comptes_bancaire->comptes->created_at->format('d-M-Y')}}</td>
+                    <td class="m-auto text-center bg-white border-2 border-solid">{{$transaction->created_at->format('d-M-Y')}}</td>
                     <td class="m-auto text-center border-2 border-solid {{$class_value}}">{{$transaction->etat_transactions->label}}</td>
-                        @if ($transaction->etat_transactions->label != "Terminer" && $transaction->etat_transactions->label != "Annuler")
+
                             <td class="m-auto text-center border-none">
                                <a  href="{{
                                 route('transactions', ['id_compte_envoyeur' => $transaction->id_compte_envoyeur]) }}" class="bouton"> Voir </a>
-
-
-
-                        @endif
 
                     </tr>
                     @endforeach
