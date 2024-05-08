6756e17bf40dabc2d3a67d5f8498eb54f61ddef2
Joel Tidjane <tidjanejoel@gmail.com>
2024-05-08 17:48:20 -0400

    Correction des bugs liés aux contraintes de dépôt et rétrait

--- resources/views/transaction/transaction.blade.php
+++ resources/views/transaction/transaction.blade.php
@@ -3,7 +3,7 @@
     <div class="flex flex-row"><!-- Premier bloc contenant message, infos et datetime -->
 
             <div class="w-2/4 text-left ">
-                <h2 class="text-3xl font-bold text-white mx-9">Détails de la transation <br>Client :  {{$transaction->comptes_bancaire->comptes->prenom}} {{$transaction->comptes_bancaire->comptes->nom}} <br>
+                <h2 class="text-3xl font-bold text-white mx-9">Détails de la transation<br>
                     Transaction N° : {{$transaction->id}}
                 </h2>
             </div>
@@ -27,8 +27,14 @@
 
     <div class="flex flex-row my-5 mx-9"><!-- Bloc détails -->
 
+
+
+
         <div class="w-2/4 "><!-- Bloc expéditeur -->
-        <p class="mt-10 mb-5 text-2xl font-bold text-white underline">Expéditeur</p>
+
+            <p class="mt-10 mb-5 text-2xl font-bold text-white underline">Expéditeur</p>
+
+            @if ($transaction->id_compte_envoyeur!=null)
             <form action="#">
                 <p class="flex items-center mb-5">
                     <label for="nom" class="w-64 text-xl text-white">Nom :  </label>
@@ -76,13 +82,22 @@
 
             </form>
 
-        </div>
+            @else
+                <p class="flex items-center mb-5 text-xl italic text-white">
+                    Le compte expediteur n'a pas été trouvé. car l'utilisateur à effectué est dépôt.<br>
+                </p>
 
+            @endif
+
+
+        </div>
 
 
         <div class="w-2/4"><!-- Bloc destinataire -->
 
             <p class="mt-10 mb-5 text-2xl font-bold text-white underline">Destinataire</p>
+
+            @if($transaction->id_compte_receveur != null)
 
             <form action="#">
                 <p class="flex items-center mb-5">
@@ -106,10 +121,49 @@
                     <input type="text" name="nom" value="{{$transaction->comptes_bancaire_receveur->nom}}" class="text-xl" disabled>
                 </p>
 
+                <p class="flex items-center mb-5">
+                    <label for="nom" class="w-64 text-xl text-white">Type de compte :  </label>
+                    <input type="text" name="nom" value="{{$transaction->comptes_bancaire_receveur->nom}}" class="text-xl" disabled>
+                </p>
+
+                <p class="flex items-center mb-5">
+                    <label for="nom" class="w-64 text-xl text-white">Montant :  </label>
+                    <input type="text" name="nom" value="{{$transaction->montant}}" class="text-xl" disabled>
+                </p>
+
+
+                <p class="flex items-center mb-5">
+                    <label for="nom" class="w-64 text-xl text-white">Etat de la transaction :  </label>
+                    <input type="text" name="nom" value="{{$transaction->etat_transactions->label}}" class="text-xl" disabled>
+                </p>
+
+
+
+                <p class="flex items-center mb-5">
+                    <label for="nom" class="w-64 text-xl text-white">Date de la transaction :  </label>
+                    <input type="text" name="nom" value="{{$transaction->created_at->format('d-M-Y')}}" class="text-xl" disabled>
+                </p>
+
+
 
             </form>
 
+
+
+            @else
+
+                <p class="flex items-center mb-5 text-xl italic text-white">
+                    Le compte destinataire n'a pas été trouvé car l'utilisateur à fait un retrait.<br>
+                </p>
+
+            @endif
+
+
         </div>
+
+
+
+
 
     </div>
 
@@ -117,8 +171,6 @@
     <div class="flex justify-start">
         <a href="{{ route('accueil') }}"><button class="bouton">Retour à l'accueil</button></a>
     </div>
-
-
 
 
 
