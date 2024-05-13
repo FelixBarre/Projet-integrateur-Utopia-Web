87e58031883b913f483e7f1378a56c18295a4569
Joel Tidjane <tidjanejoel@gmail.com>
2024-05-13 13:11:21 -0400

    MAJ de transactionResource

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -333,11 +333,14 @@ class TransactionController extends Controller
 
             if (empty($dateDebut)) {
 
-                return back()->with('error', 'Veuillez choisir une date de début.');
+                session()->flash('erreur', 'Vous devez choisir une date de début');
+                return redirect(route('accueil'));
             }
 
             if (!empty($dateFin) && $dateFin < $dateDebut) {
-                return back()->with('error', 'La date de fin doit être ultérieure à la date de début.');
+                session()->flash('erreur', 'La date de fin doit être ultérieure à la date de début.');
+                return redirect(route('accueil'));
+
             }
             $transactions = Transaction::whereDate('created_at', '>=', $dateDebut)
                             ->whereDate('created_at', '<=', $dateFin)
