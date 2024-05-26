3136888da62c4d08d6a81cdd30fac69585026be2
Joel Tidjane <tidjanejoel@gmail.com>
2024-05-26 14:46:48 -0400

    MAJ de la vue transaction

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -16,7 +16,7 @@ class TransactionSeeder extends Seeder
 
     DB::table('transactions')->insert([
         ['montant' => 500,
-        'id_compte_envoyeur' => 1,
+        'id_compte_envoyeur' => null,
         'id_compte_receveur' => 1,
         'id_type_transaction' =>1,
         'id_etat_transaction' =>2,
@@ -24,23 +24,16 @@ class TransactionSeeder extends Seeder
         'created_at' => date('Y-m-d H:i:s')],
         ['montant' => 1500,
         'id_compte_envoyeur' => 1,
-        'id_compte_receveur' => 1,
+        'id_compte_receveur' => null,
         'id_type_transaction' =>2,
         'id_etat_transaction' =>3,
         'id_facture' =>null,
         'created_at' => date('Y-m-d H:i:s')],
         ['montant' => 2800,
         'id_compte_envoyeur' => 1,
-        'id_compte_receveur' => 1,
+        'id_compte_receveur' => 2,
         'id_type_transaction' =>3,
         'id_etat_transaction' =>1,
-        'id_facture' =>null,
-        'created_at' => date('Y-m-d H:i:s')],
-        ['montant' => 280,
-        'id_compte_envoyeur' => 1,
-        'id_compte_receveur' => 1,
-        'id_type_transaction' =>1,
-        'id_etat_transaction' =>3,
         'id_facture' =>null,
         'created_at' => date('Y-m-d H:i:s')],
         ['montant' => 1280,
@@ -48,7 +41,7 @@ class TransactionSeeder extends Seeder
         'id_compte_receveur' => null,
         'id_type_transaction' =>4,
         'id_etat_transaction' =>3,
-        'id_facture' =>1,
+        'id_facture' =>3,
         'created_at' => date('Y-m-d H:i:s')]
         ]);
     }
